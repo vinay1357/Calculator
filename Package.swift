cca05534c07676668da2479a7dc5bee33d21ea1f
vinay1357 <vinaykamra02@gmail.com>
2023-06-30 03:05:36 +0530

    Update swift tool version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Calculator",
     platforms: [
-           .iOS(.v15)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -15,12 +15,12 @@ let package = Package(
             targets: ["Calculator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vinay1357/ThemeKit.git", .branch("main")),
+        .package(url: "https://github.com/vinay1357/ThemeKit.git", branch: "main"),
         
-        .package(url: "https://github.com/vinay1357/CalculatorUIComponent", .branch("main"))
-
+            .package(url: "https://github.com/vinay1357/CalculatorUIComponent" , branch:"main")
+        
     ],
-
+    
     
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -38,5 +38,5 @@ let package = Package(
             dependencies: ["Calculator"]),
     ],
     swiftLanguageVersions: [.v5]
-
+    
 )
